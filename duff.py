bf74415443590029b20a2bf88565b4e63ef73a23
Rajsinghc <60809191+Rajsinghc@users.noreply.github.com>
2020-02-08 13:17:48 +0530

    Add files via upload

---
+++ duff.py
@@ -0,0 +1,67 @@
+import turtle
+x = turtle.Turtle()
+x.speed(70)
+l = 70
+for i in range (13):
+	x.circle(l)
+	x.circle(l - 5)
+	x.circle(l - 10)
+	x.circle(l - 15)
+	x.circle(l - 20)
+	x.circle(l - 25)
+	x.lt(30)
+l = l + 5
+x.pencolor("red")
+for i in range (13):
+	x.circle(l)
+	x.circle(l - 5)
+	x.circle(l - 10)
+	x.circle(l - 15)
+	x.circle(l - 20)
+	x.circle(l - 25)
+	x.lt(30)
+l = l + 5
+x.pencolor("blue")
+for i in range (13):
+	x.circle(l)
+	x.circle(l - 5)
+	x.circle(l - 10)
+	x.circle(l - 15)
+	x.circle(l - 20)
+	x.circle(l - 25)
+	x.lt(30)
+l = l + 5
+x.pencolor("green")
+for i in range (13):
+	x.circle(l)
+	x.circle(l - 5)
+	x.circle(l - 10)
+	x.circle(l - 15)
+	x.circle(l - 20)
+	x.circle(l - 25)
+	x.lt(30)
+l = l + 5
+x.pencolor("yellow")
+for i in range (13):
+	x.circle(l)
+	x.circle(l - 5)
+	x.circle(l - 10)
+	x.circle(l - 15)
+	x.circle(l - 20)
+	x.circle(l - 25)
+	x.lt(30)
+l = l + 5
+x.pencolor("orange")
+for i in range (13):
+	x.circle(l)
+	x.circle(l - 5)
+	x.circle(l - 10)
+	x.circle(l - 15)
+	x.circle(l - 20)
+	x.circle(l - 25)
+	x.lt(30)
+x.width(0)
+x.pencolor("white")
+x.lt(90)
+x.fd(250)
+x.write("how's that")
