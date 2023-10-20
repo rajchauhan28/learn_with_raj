1b38f9608d4697a18754ebd00599234876f3615e
Raj singh chauhan <67595118+rajchauhan28@users.noreply.github.com>
2023-10-20 18:56:49 +0530

    Update random_dice.py

--- random_dice.py
+++ random_dice.py
@@ -6,54 +6,54 @@ while True:
 	if r==1:
 		print('''
 		______________
-		l                       l
-		l                       l
-		l         @           l
-		l                       l
+		l             l
+		l             l
+		l      @      l
+		l             l
 		l_____________l
 		''')
 	elif r==2:
 		print('''
 		______________
-		l                       l
-		l                       l
-		l    @        @    l
-		l                       l
+		l             l
+		l             l
+		l   @     @   l
+		l             l
 		l_____________l
 		''')
 	elif r==3:
 		print('''
 		______________
-		l                       l
-		l                       l
-		l  @     @    @  l
-		l                       l
+		l             l
+		l          @  l
+		l      @      l
+		l  @          l
 		l_____________l
 		''')
 	elif r==4:
 		print('''
 		______________
-		l                       l
-		l   @           @  l
-		l                       l
-		l   @           @  l
+		l             l
+		l   @      @  l
+		l             l
+		l   @      @  l
 		l_____________l
 		''')
 	elif r==5:
 		print('''
 		______________
-		l                       l
-		l   @          @   l
-		l         @          l
-		l   @          @   l
+		l             l
+		l  @       @  l
+		l      @      l
+		l   @      @  l
 		l_____________l
 		''')
 	elif r==6:
 		print('''
 		______________
-		l                       l
-		l  @            @  l
-		l  @            @  l
-		l  @            @  l
+		l             l
+		l  @       @  l
+		l  @       @  l
+		l  @       @  l
 		l_____________l
 		''')
